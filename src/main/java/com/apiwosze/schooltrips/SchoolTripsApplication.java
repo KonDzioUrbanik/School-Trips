dd7a6e42f7b65b995ac4c2edef799c3d52457afd
KonDzioUrbanik <konradsparta@o2.pl>
2025-11-28 16:02:15 +0100

    first commit

---
+++ src/main/java/com/apiwosze/schooltrips/SchoolTripsApplication.java
@@ -0,0 +1,13 @@
+package com.apiwosze.schooltrips;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SchoolTripsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SchoolTripsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/apiwosze/schooltrips/SchoolTripsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.apiwosze.schooltrips;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SchoolTripsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
