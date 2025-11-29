86f15f000f154a7c6f12ce31cf7e379320897191
KonDzioUrbanik <konradsparta@o2.pl>
2025-11-29 13:38:57 +0100

    CRUD do Klasy

--- src/main/java/com/apiwosze/schooltrips/klasa/KlasaService.java
+++ src/main/java/com/apiwosze/schooltrips/klasa/KlasaService.java
@@ -1,8 +1,9 @@
 package com.apiwosze.schooltrips.klasa;
 
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
 
 @Service                                            //Tutaj mamy całą "Logikę biznesową" czyli tak na prawdę najwięcej kodu
 public class KlasaService {                         //Będziemy opisywać tutaj to co ma robić dany moduł czyli to co nie daje nam JPA czyli potrzebne funkcje
@@ -27,7 +28,7 @@ public class KlasaService {                         //Będziemy opisywać tutaj 
         }
     }
 
-    @Transactional
+    @Transactional          //wymagane przez springa bo on ogarnia tylko usuwwanie po id
     public void deleteKlasa(KlasaDto klasaDto){
         if(!klasaRepository.existsByNazwa(klasaDto.nazwa())){
             throw new IllegalArgumentException("Nie ma takiej klasy!");
@@ -36,6 +37,25 @@ public class KlasaService {                         //Będziemy opisywać tutaj 
             klasaRepository.deleteByNazwa(klasaDto.nazwa());
         }
     }
+
+    public List<KlasaModel> getAllKlasy(){
+        return klasaRepository.findAll();
+    }
+
+    public KlasaModel updateKlasy(KlasaDto klasaDto){       //zmiana profilu
+        KlasaModel klasaModelEdit = klasaRepository.findByNazwa(klasaDto.nazwa())
+                .orElseThrow(() -> new IllegalArgumentException("Nie ma takiej klasy!"));
+        klasaModelEdit.setProfil(klasaDto.profil());
+        return klasaRepository.save(klasaModelEdit);
+    }
+    public KlasaModel updateNazwyKlasy(Long id, KlasaDto klasaDto){
+        KlasaModel klasaModelEdit = klasaRepository.findById(id)
+                .orElseThrow(() -> new IllegalArgumentException("Nie ma takiej klasy!"));
+        klasaModelEdit.setNazwa(klasaDto.nazwa());
+        return klasaRepository.save(klasaModelEdit);
+    }
+
+
 
 
 }
