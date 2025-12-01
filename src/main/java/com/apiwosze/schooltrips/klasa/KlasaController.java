42bcf3ccd192c5800cf0e86ec99c0eb48679bb44
KonDzioUrbanik <konradsparta@o2.pl>
2025-12-01 12:54:34 +0100

    Dodanie Opis do swaggera

--- src/main/java/com/apiwosze/schooltrips/klasa/KlasaController.java
+++ src/main/java/com/apiwosze/schooltrips/klasa/KlasaController.java
@@ -1,5 +1,7 @@
 package com.apiwosze.schooltrips.klasa;
 
+import io.swagger.v3.oas.annotations.Operation;
+import jdk.jfr.Description;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -13,25 +15,32 @@ public class KlasaController {
         this.klasaService = klasaService;
     }
 
+
+    @Operation(summary = "Dodawanie nowej klasy")
     @PostMapping        //endpoint do tworzenia klasy
     public KlasaModel createKlasa(@RequestBody KlasaDto klasaDto){ //Ta adnotacja mówi springowi że ma szukać w body/JSON a nie w adres URL
        return klasaService.createKlasa(klasaDto);
     }
 
+    @Operation(summary = "Usuwanie klasy")
     @DeleteMapping      //endpoint do usuwania klasy
     public void deleteKlasa(@RequestBody KlasaDto klasaDto){
         klasaService.deleteKlasa(klasaDto);
     }
 
+    @Operation(summary = "Pobieranie wszystkich klas")
     @GetMapping         //endpoint do pobrania wszystkich klas
     public List<KlasaModel> getAllKlasy(){
         return klasaService.getAllKlasy();
     }
+
+    @Operation(summary = "Aktualizacja profilu klasy")
     @PutMapping         //endpoint do aktualizacji profilu
     public KlasaModel updateProfil(@RequestBody KlasaDto klasaDto) {
         return klasaService.updateProfilKlasy(klasaDto);
     }
 
+    @Operation(summary = "Aktualizacja nazwy klasy")
     @PutMapping("/{id}")    //endpoint do aktualizacji nazwy klasy
     public KlasaModel updateNazwa(@PathVariable Long id, @RequestBody KlasaDto klasaDto) {
         return klasaService.updateNazwyKlasy(id, klasaDto);
